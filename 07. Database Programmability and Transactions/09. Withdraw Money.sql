6f90e5bcb179d655831eae5a57c1243cae7f7244
Kris Zeynev <kriszeynev@yahoo.com>
2025-10-09 21:37:38 +0300

    09. Withdraw Money

---
+++ 07. Database Programmability and Transactions/09. Withdraw Money.sql
@@ -0,0 +1,29 @@
+CREATE OR REPLACE PROCEDURE sp_withdraw_money(
+    account_id INT,
+    money_amount NUMERIC(4)
+)
+AS
+    $$
+        DECLARE
+            account_balance NUMERIC;
+    BEGIN
+            SELECT
+                balance
+            FROM
+                accounts
+            WHERE
+                id = account_id
+            INTO
+                account_balance;
+
+            IF account_balance - money_amount >= 0 THEN
+                UPDATE accounts
+                SET balance = balance - money_amount
+                WHERE id = account_id;
+                COMMIT;
+            ELSE
+                raise notice 'Insufficient balance to withdraw %', account_balance;
+            END IF;
+    END;
+    $$
+LANGUAGE plpgsql;
